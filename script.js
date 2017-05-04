881af2381cc5924cf059806139ce823e35b3c052
Sayam Kanwar <sayamkanwar616@gmail.com>
2017-05-05 04:11:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-videos = document.querySelectorAll("video");
-for (var i = 0, l = videos.length; i < l; i++) {
-    var video = videos[i];
-    var src = video.src || (function () {
-        var sources = video.querySelectorAll("source");
-        for (var j = 0, sl = sources.length; j < sl; j++) {
-            var source = sources[j];
-            var type = source.type;
-            var isMp4 = type.indexOf("mp4") != -1;
-            if (isMp4) return source.src;
+//youtube script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('player', {
+        height: '244',
+        width: '434',
+        videoId: 'AkyQgpqRyBY',  // youtube video id
+        playerVars: {
+            'autoplay': 0,
+            'rel': 0,
+            'showinfo': 0
+        },
+        events: {
+            'onStateChange': onPlayerStateChange
         }
-        return null;
-    })();
-    if (src) {
-        var isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
-        if (isYoutube) {
-            var id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
-            id = (id.length > 1) ? id.splice(1) : id;
-            id = id.toString();
-            var mp4url = "http://www.youtubeinmp4.com/redirect.php?video=";
-            video.src = mp4url + id;
-        }
+    });
+}
+
+onPlayerStateChange = function (event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        $('.start-video').fadeIn('normal');
     }
 }
+
+$(document).on('click', '.start-video', function () {
+    $(this).fadeOut('normal');
+    player.playVideo();
+});
+
 
 function endLoad() {
   document.getElementById('loading').style.opacity=0;

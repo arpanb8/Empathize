d0462df65ca671a052b7d80f66cf4adfa4bf9f8c
Sayam Kanwar <sayamkanwar616@gmail.com>
2017-05-05 04:15:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-//youtube script
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-
-onYouTubeIframeAPIReady = function () {
-    player = new YT.Player('player', {
-        height: '100',
-        width: '100',
-        videoId: 'pHRvWH2bLQk',  // youtube video id
-        playerVars: {
-            'autoplay': 0,
-            'rel': 0,
-            'showinfo': 0
-        },
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-onPlayerStateChange = function (event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        $('.play-btn').fadeIn('normal');
-    }
-}
-
-$(document).on('click', '.play-btn', function () {
-    $(this).fadeOut('normal');
-    player.playVideo();
-});
-
-
 function endLoad() {
   document.getElementById('loading').style.opacity=0;
   document.getElementById('loading').style.zIndex=-1;
